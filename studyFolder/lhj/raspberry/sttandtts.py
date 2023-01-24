8ad28dcb058fc8c3d4b968d0427ddc6cb0b6fb43
gkrwns1901 <gkrwns1901@naver.com>
2023-01-24 23:02:55 +0900

    add: rapberry pi file
    Former-commit-id: a45ffe2899b7d373cab35c98b248b3cc3a0bb12f

--- studyFolder/lhj/raspberry/sttandtts.py
+++ studyFolder/lhj/raspberry/sttandtts.py
@@ -1,9 +1,12 @@
+import pyttsx3
 import speech_recognition as sr
 
 r = sr.Recognizer()
+s = pyttsx3.init()
 with sr.Microphone() as source:
     print("Say something! : ")
     audio = r.listen(source)
-# print( r.recognize_google(audio, language='ko-KR'))
 text = r.recognize_google(audio, language='ko-KR')
 print(text)
+s.say(text)
+s.runAndWait()
