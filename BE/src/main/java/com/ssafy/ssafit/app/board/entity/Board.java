acd5ec906cf1caa92a65e9a6e5faeeb918b392b0
HanSunHee <xoa1235@naver.com>
2023-01-25 14:31:11 +0900

    feat: Add BE

---
+++ BE/src/main/java/com/ssafy/ssafit/app/board/entity/Board.java
@@ -0,0 +1,7 @@
+package com.ssafy.ssafit.app.board.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+public class Board {
+}
---
+++ BE/src/main/java/com/ssafy/ssafit/app/exercise/controller/ExerciseController.java
@@ -0,0 +1,7 @@
+package com.ssafy.ssafit.app.exercise.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExerciseController {
+}
