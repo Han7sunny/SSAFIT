67368bb2e92961dc9b52aed701f967a950f02605
강권우 <kunwoo3121@naver.com>
2023-02-16 14:14:37 +0900

    파일 업로드 2차 도전

--- BE/src/main/java/com/ssafy/ssafit/app/user/service/UserServiceImpl.java
+++ BE/src/main/java/com/ssafy/ssafit/app/user/service/UserServiceImpl.java
@@ -312,7 +312,7 @@ public class UserServiceImpl implements UserService{
 
     public String uploadImage(MultipartFile file) throws Exception{
         UUID uuid = UUID.randomUUID();
-        final String FOLDER_PATH = System.getProperty("user.home") + "/upload_files/";
+        final String FOLDER_PATH = "C:\\SSAFY\\";
 
         String filePath = FOLDER_PATH + uuid.toString() + "_" + file.getOriginalFilename();
             // 파일 결로
