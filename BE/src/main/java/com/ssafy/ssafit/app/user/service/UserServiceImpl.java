85efc457c8646a323730fa59f8da363b4cd98b06
강권우 <kunwoo3121@naver.com>
2023-02-16 16:38:16 +0900

    파일 업로드 5차 도전

--- BE/src/main/java/com/ssafy/ssafit/app/user/service/UserServiceImpl.java
+++ BE/src/main/java/com/ssafy/ssafit/app/user/service/UserServiceImpl.java
@@ -26,6 +26,8 @@ import com.ssafy.ssafit.util.Sha256;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 import com.ssafy.ssafit.app.user.dto.req.UserJoinReqDto;
@@ -315,12 +317,15 @@ public class UserServiceImpl implements UserService{
         final String FOLDER_PATH = System.getProperty("file.separator") + "home" + System.getProperty("file.separator") + "ubuntu" + System.getProperty("file.separator") + "photo" + System.getProperty("file.separator");
 
         String filePath = FOLDER_PATH + uuid.toString() + "_" + file.getOriginalFilename();
-            // 파일 결로
-        File destFile = new File(filePath);
-        file.transferTo(destFile);
 
-        destFile.setWritable(true);
-        destFile.setReadable(true);
+        Path path = Paths.get(filePath).toAbsolutePath();
+        file.transferTo(path.toFile());
+            // 파일 결로
+//        File destFile = new File(filePath);
+//        file.transferTo(destFile);
+//
+//        destFile.setWritable(true);
+//        destFile.setReadable(true);
 
 //        String enc_path = filePath.replace("\\", "/");
 //        String enc_value = saveFaceEncoding(filePath);
