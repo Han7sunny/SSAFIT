e1c09730eac1baca0b01b84927590c61bdb368df
강권우 <kunwoo3121@naver.com>
2023-02-16 11:03:47 +0900

    .

--- BE/src/main/java/com/ssafy/ssafit/mirror/controller/MirrorController.java
+++ BE/src/main/java/com/ssafy/ssafit/mirror/controller/MirrorController.java
@@ -209,7 +209,7 @@ public class MirrorController {
 
 
 
-    
+
     @GetMapping("/get-challenge-time")
     @ApiOperation(value = "챌린지 모드 순위 정보", notes = "챌린지 모드 순위 정보를 받아옵니다.", response = MirrorChallengeTimeRespDto.class)
     public ResponseEntity<?> getChallengeTime(@AuthenticationPrincipal CustomUserDetails user) {
